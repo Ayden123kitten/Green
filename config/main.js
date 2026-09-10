92bf90df7244ff1b82bfcbdf45e557fba7e4d6cb
Ayden123kitten <ayden12213223platypu@gmail.com>
2026-09-10 11:15:40 -0400

    C

--- config/main.js
+++ config/main.js
@@ -5,11 +5,11 @@ document.title = `${document.title} | ${sitename}`;
 document.getElementById("title").innerHTML = sitename;
 document.getElementById("subtitle").innerHTML = subtext;
 
-let gamesData = []; 
+let gamesData = [];
 
 function displayFilteredGames(filteredGames) {
   const gamesContainer = document.getElementById("gamesContainer");
-  gamesContainer.innerHTML = ""; 
+  gamesContainer.innerHTML = "";
 
   filteredGames.forEach((game) => {
     const gameDiv = document.createElement("div");
@@ -18,9 +18,9 @@ function displayFilteredGames(filteredGames) {
     const gameImage = document.createElement("img");
     gameImage.src = game.image;
     gameImage.alt = game.name;
-    
+
     gameImage.onclick = () => {
-      window.location.href = `play.html?gameurl=${encodeURIComponent(game.url)}`;
+      window.location.href = `/${game.slug}`;
     };
 
     const gameName = document.createElement("p");
@@ -33,19 +33,23 @@ function displayFilteredGames(filteredGames) {
 }
 
 function handleSearchInput() {
-  const searchInputValue = document.getElementById("searchInput").value.toLowerCase();
+  const searchInputValue = document
+    .getElementById("searchInput")
+    .value.toLowerCase();
   const filteredGames = gamesData.filter((game) =>
     game.name.toLowerCase().includes(searchInputValue)
   );
   displayFilteredGames(filteredGames);
 }
 
-fetch("/config/games.json") 
+fetch("/config/games.json")
   .then((response) => response.json())
   .then((data) => {
     gamesData = data;
-    displayFilteredGames(data); 
+    displayFilteredGames(data);
   })
   .catch((error) => console.error("Error fetching games:", error));
 
-document.getElementById("searchInput").addEventListener("input", handleSearchInput);
+document
+  .getElementById("searchInput")
+  .addEventListener("input", handleSearchInput);
